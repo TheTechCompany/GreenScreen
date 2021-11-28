4bfa20ee49b8cb5acbc88c1b0d505ee220b44e21
Ross Leitch <professional.balbatross@gmail.com>
2021-11-28 21:20:24 +1300

    Display manager

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ export class GreenScreen {
 	constructor(){
 		this.displayManager = new DisplayManager('http://localhost:3000/asset1/index.html');
 		this.assetStore = new AssetStore({
-			assetStoreUrl: `http://192.168.255.66`,
+			assetStoreUrl: `http://192.168.255.66:4200`,
 			assetStoragePath: `/tmp/`
 		});
 	}
